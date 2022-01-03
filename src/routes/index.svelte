0ee02e6eb1a77edef09de3cd0f525ea5619c01bd
gcunruh <g@gabrielunruh.com>
2022-01-03 17:26:45 -0600

    contemporary subtraction

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,6 +13,7 @@
         >
           Classic
         </button>
+        <!--
         <button
           type="button"
           on:click={() => (bg = 'bg-black')}
@@ -20,17 +21,18 @@
         >
           Contemporary
         </button>
+        -->
         <button
           type="button"
           on:click={() => (bg = 'bg-white')}
           class="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
         >
-          Minimalist
+          Modern
         </button>
       </span>
     </div>
     <img
-      class="w-full my-5"
+      class="w-full my-5 mx-auto max-w-screen-md"
       alt="a painting with frame"
       src={bg === 'bg-yellow-100'
         ? './lone-classic.png'
