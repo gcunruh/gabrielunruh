448e7117884f22f96194dde6ca30310fefb02b92
gcunruh <49214225+gcunruh@users.noreply.github.com>
2022-03-12 11:31:37 -0600

    updates 2022

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,12 +4,15 @@
 
 <div class="h-full w-full {bg}">
   <div class="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 mx-auto">
+    <div class="px-4 pt-3 text-lg font-semibold tracking-wide">
+      The Lone Minstrel
+    </div>
     <div class="px-4 pt-3">
       <span class="my-2 z-0 inline-flex shadow-sm rounded-md">
         <button
           type="button"
           on:click={() => (bg = 'bg-amber-100')}
-          class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500"
+          class="relative inline-flex items-center px-4 py-1 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500"
         >
           Classic
         </button>
@@ -25,7 +28,7 @@
         <button
           type="button"
           on:click={() => (bg = 'bg-gray-100')}
-          class="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500"
+          class="-ml-px relative inline-flex items-center px-4 py-1 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500"
         >
           Modern
         </button>
@@ -44,7 +47,7 @@
             ? './lone-con.png'
             : ''}
         />
-        <div class="my-5 relative text-left">
+        <!-- <div class="my-5 relative text-left">
           <img
             class="h-28 md:h-40 drop-shadow-md"
             alt="A plaque with a painting"
@@ -63,7 +66,7 @@
               >The Royal Trust</span
             >
           </div>
-        </div>
+        </div> -->
       </div>
       <!-- <div class="text-black font-bold w-full md:w-3/4 px-4 md:px-0">
       Other Works of Art
@@ -74,6 +77,25 @@
         <div class="w-1/2">3</div>
       </div>
     </div> -->
+    </div>
+    <h1 class="px-4 pt-3 font-semibold" >
+      Other Works of Art (Excuse our dust, work in progress)
+    </h1>
+    <div class="px-4 pt-3 grid grid-cols-2 gap-3">
+      <div class={"rounded-sm flex flex-row h-12 w-auto items-center justify-center " + (bg === 'bg-amber-100' ? "border-4 border-amber-700" : "border-4 border-black")}>
+          <a class="cursor-pointer" href="https://thestreamingguide.com" target="_blank">
+          TSG</a>
+      </div>
+      <div class={"rounded-sm flex flex-row h-12 w-auto items-center justify-center " + (bg === 'bg-amber-100' ? "border-4 border-amber-700" : "border-4 border-black")}>
+        <a class="cursor-pointer" href="https://mushroom.party" target="_blank">
+          Mushroom Party</a>
+      </div>
+      <div class={"rounded-sm flex flex-row h-12 w-auto items-center justify-center " + (bg === 'bg-amber-100' ? "border-4 border-amber-700" : "border-4 border-black")}>
+        Founder Anonymous
+      </div>
+      <div class={"rounded-sm flex flex-row h-12 w-auto items-center justify-center " + (bg === 'bg-amber-100' ? "border-4 border-amber-700" : "border-4 border-black")}>
+        Scacchi
+      </div>
     </div>
     <div class="h-80" />
   </div>
